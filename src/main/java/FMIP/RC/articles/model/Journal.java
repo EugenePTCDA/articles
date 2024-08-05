35414b44a33415b68033028fa6fb8b3c740d8c9c
eugene <eugene.dbv@gmail.com>
2024-08-05 15:14:42 +0300

    add Journal model

--- src/main/java/FMIP/RC/articles/model/Journal.java
+++ src/main/java/FMIP/RC/articles/model/Journal.java
@@ -18,7 +18,7 @@ public class Journal {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-    private String researchDirection;
-    private String core;
+    private String journalName;
+    private String researchTopic;
+    private String citationIndex;
 }
