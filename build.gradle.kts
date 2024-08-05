9ea32dbf31dde26cbe41fa7d69dbc16e7c80c425
eugene <eugene.dbv@gmail.com>
2024-08-05 15:14:05 +0300

    refactor kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
+	application
 }
 
 group = "FMIP.RC"
@@ -13,10 +14,8 @@ java {
 	}
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+application {
+	mainClass.set("FMIP.RC.articles.ArticlesApplication")
 }
 
 repositories {
@@ -26,15 +25,22 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.projectlombok:lombok:1.18.24")
-	annotationProcessor("org.projectlombok:lombok:1.18.24")
+	implementation("org.projectlombok:lombok")
+	implementation("javax.validation:validation-api:2.0.1.Final") // Specify the version
+	implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final") // Specify the version
 	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("org.mockito:mockito-junit-jupiter")
+	annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		events("passed", "skipped", "failed")
+	}
 }
